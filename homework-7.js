5b5e3704a848b2523a9e9451905efe2aef11676d
Azamat <abusuleiman.azamat@gmail.com>
2026-07-15 21:56:33 +0300

    fix indentation

--- homework-7.js
+++ homework-7.js
@@ -1,9 +1,9 @@
 function showTemperature(city, temperature) {
   console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию`)
-}
+};
 showTemperature('Иваново', 21);
 
-const LIGHT_SPEED =  299792458
+const LIGHT_SPEED =  299792458;
 function checkSpeed(speed) {
   if (speed >  LIGHT_SPEED) {
     console.log('Сверхсветовая скорость');
@@ -12,18 +12,18 @@ function checkSpeed(speed) {
   } else {
    console.log('Субсветовая скорость');
   }
-}
+};
 checkSpeed(299792458);
 
 let product = "Iphone";
 let price = 100;
 function checkPrice(balance) {
-  if( balance >= price) {
+  if (balance >= price) {
     console.log(`${product} приобретён. Спасибо за покупку!`); 
   }
   else {
     const difference = price - balance;
     console.log(`Вам не хватает ${difference}$, пополните баланс`);
   }
-}
-checkPrice(90)
+};
+checkPrice(90);
