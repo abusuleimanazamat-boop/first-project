3068b5f9c4a43ac0f5e7665954e47d8cd070e842
Azamat <abusuleiman.azamat@gmail.com>
2026-08-24 22:42:30 +0300

    Fix review comments

--- homework-10.js
+++ homework-10.js
@@ -3,26 +3,6 @@ import { productCards } from "./product-cards.js";
 const productCardTemplate = document.querySelector('#product-card-template');
 const products = document.querySelector('#products');
 
-productCards.forEach(product => {
-  const productCopy = productCardTemplate.content.cloneNode(true);
-  productCopy.querySelector('.product-card__title').textContent = product.title;
-  productCopy.querySelector('.product-card__description').textContent = product.description;
-  productCopy.querySelector('.product-card__skin-type').textContent = product.skinType;
-  productCopy.querySelector('.product-card__ingredients-title').textContent = `Состав`;
-  productCopy.querySelector('.product-card__price').textContent = product.price;
-  productCopy.querySelector('.product-card__image').src = product.image;
-  const compoundLists = productCopy.querySelector('.product-card__ingredients');
-
-  product.ingredients.forEach(ingredient => {
-    const li = document.createElement('li')
-    li.textContent = ingredient
-    compoundLists.appendChild(li)
-
-  });
-  products.appendChild(productCopy);
-});
-console.log(productCards);
-
 const productDescription = productCards.reduce((acc, product) => {
   acc.push({ [product.title]: product.description });
   return acc;
@@ -39,3 +19,27 @@ function showCards() {
   }
   return count;
 };
+
+function renderProductCards(productCards) {
+  productCards.forEach(({ title, description, skinType, price, image,ingredients,}) => {
+    const productCopy = productCardTemplate.content.cloneNode(true);
+    productCopy.querySelector('.product-card__title').textContent = title;
+    productCopy.querySelector('.product-card__description').textContent = description;
+    productCopy.querySelector('.product-card__skin-type').textContent = skinType;
+    productCopy.querySelector('.product-card__price').innerHTML = `${price} &#8381;`;
+    productCopy.querySelector('.product-card__image').src = `./img/${image}.png`;
+    const compoundLists = productCopy.querySelector('.product-card__ingredients');
+    
+    ingredients.forEach(ingredient => {
+      const li = document.createElement('li');
+      li.textContent = ingredient;
+      compoundLists.appendChild(li);
+      
+      products.appendChild(productCopy);
+    });
+  });
+  
+};
+const result = showCards();
+const cardsToRender = productCards.slice(0, result);  console.log(cardsToRender);
+renderProductCards(cardsToRender);
